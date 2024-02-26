60f8b87a2769bd3f35e8663d518b0a249dc66e0d
Ryan David Ward <ryandward@gmail.com>
2024-02-25 21:04:09 -0600

    removed redundancy

--- initBot.ts
+++ initBot.ts
@@ -1,5 +1,6 @@
 // botInit.ts
 import { Client, GatewayIntentBits, REST } from "discord.js";
+import 'dotenv/config';
 import { logger } from "./Logger.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -12,26 +13,33 @@ type Env = {
 };
 
 // Function to check environment variables
-function checkEnvVariables(env: typeof process.env): env is Env {
-    return Boolean(env.DISCORD_TOKEN && env.BOT_SELF && env.GUILD_ID);
+function checkEnvVariables(env: typeof process.env): Promise<Env> {
+  return new Promise((resolve, reject) => {
+    if (env.DISCORD_TOKEN && env.BOT_SELF && env.GUILD_ID) {
+      resolve(env as Env);
+    } else {
+      reject(new Error("Required environment variables are not set"));
+    }
+  });
 }
 
 async function initBot() {
-    logger.info("Started bot initialization");
+  logger.info("Started bot initialization");
 
-    if (!checkEnvVariables(process.env)) {
-        logger.error("Required environment variables are not set");
-        throw new Error("Required environment variables are not set");
-    }
+  checkEnvVariables(process.env)
+    .then((env) => {
+      const rest = new REST({ version: "10" }).setToken(env.DISCORD_TOKEN);
 
-    const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
-
-    client.login(process.env.DISCORD_TOKEN)
+      client.login(env.DISCORD_TOKEN)
         .then(() => logger.info("Finished bot initialization"))
         .catch((error) => {
-            logger.error("Failed to login", error);
-            throw error; // Rethrow or handle as appropriate
+          logger.error("Failed to login", error);
+          throw error; 
         });
+    })
+    .catch((error) => {
+      logger.error(error.message);
+      throw error; 
+    });
 }
-
 export { client, initBot };
