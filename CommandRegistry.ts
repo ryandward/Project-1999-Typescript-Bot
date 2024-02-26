1bf2ce9bd15032e6f636f84bbd1d4511c9851d24
Ryan David Ward <ryandward@gmail.com>
2024-02-25 21:03:16 -0600

    strategy for each option to autocomplete

--- CommandRegistry.ts
+++ CommandRegistry.ts
@@ -1,7 +1,7 @@
 // commandRegistry.ts
-import { AutocompleteInteraction, CommandInteraction } from 'discord.js';
-import { BankUtils } from './BankUtils.js';
-import { CensusUtils } from './CensusUtils.js';
+import { AutocompleteInteraction, CommandInteraction } from "discord.js";
+import { BankUtils } from "./BankUtils.js";
+import { CensusUtils } from "./CensusUtils.js";
 
 interface Command {
   name: string;
@@ -12,8 +12,11 @@ interface Command {
     description: string;
     required: boolean;
     autocomplete: boolean;
+    handleAutocomplete?: (
+      interaction: AutocompleteInteraction
+    ) => Promise<void>;
   }[];
-  autocomplete: (interaction: AutocompleteInteraction) => Promise<void>;
+  // autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>;
   execute: (interaction: CommandInteraction) => Promise<void>;
 }
 
@@ -22,4 +25,12 @@ const commands: Command[] = [];
 commands.push(...BankUtils.commands);
 commands.push(...CensusUtils.commands);
 
-export { commands };
+const commandMetadatas = commands.map(
+  ({ execute, options, ...metadata }) => ({
+    ...metadata,
+    options: options.map(({ handleAutocomplete, ...option }) => option),
+  })
+);
+
+export { commandMetadatas, commands };
+
